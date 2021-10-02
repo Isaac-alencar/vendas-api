37775241d278691a13d4574aee5a38bacfb7466d
Isaac Alencar <alencarisaac3@gmail.com>
2021-10-01 21:31:46 -0300

    chore(database): config and run first migration
    create migration for product column on database and config cli script to run migrations

---
+++ src/shared/typeorm/migrations/1633050428689-create-products-table.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createProductsTable1633050428689 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "products",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "price",
+            type: "decimal",
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: "quantity",
+            type: "int",
+          },
+          {
+            name: "created_at",
+            type: "timestamp with time zone",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp with time zone",
+            default: "now()",
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("products");
+  }
+}
